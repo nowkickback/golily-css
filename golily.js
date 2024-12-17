c20f62b0713e01bef706f6901a065451ee46e10a
Troy <raptblue@gmail.com>
2024-12-17 16:54:07 -0500

    Create golily.js

---
+++ golily.js
@@ -0,0 +1,9 @@
+"routeLoaded routeChangeEvent".split(" ").forEach(function(e){
+  window.addEventListener(e, hideOpportunityTabs, false);
+});
+
+function hideOpportunityTabs() {
+  [...document.querySelectorAll(".innerWrapper .wrapperHeader div:not(.toolkit-tab) > div.pr-4 > button")].filter(e => e.childNodes && [...e.childNodes].find(n => n.nodeValue?.match("HIDDEN"))).forEach(function(t) {
+    t.hidden = true
+  }
+}
