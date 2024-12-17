7aed5c284405e765d5a64ea48f68345eae09dc43
Troy <raptblue@gmail.com>
2024-12-17 16:57:12 -0500

    Update golily.js

--- golily.js
+++ golily.js
@@ -5,5 +5,5 @@
 function hideOpportunityTabs() {
   [...document.querySelectorAll(".innerWrapper .wrapperHeader div:not(.toolkit-tab) > div.pr-4 > button")].filter(e => e.childNodes && [...e.childNodes].find(n => n.nodeValue?.match("HIDDEN"))).forEach(function(t) {
     t.hidden = true
-  }
+  })
 }
